339e9d68590fc9938b9e884874709c4b052f3e0a
front-sio <masanja.developer@gmail.com>
2023-02-21 16:07:40 +0300

    added install_app

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,6 +50,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'office',
+    'visitor',
+    'customer',
+    'avocado',
+    'reception',
+    'accounts',
 ]
 
 MIDDLEWARE = [
