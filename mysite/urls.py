9a21e719c91926bf37f788c11dcb63c85db40d0a
front-sio <masanja.developer@gmail.com>
2023-02-21 16:41:36 +0300

    front page added

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
     path('customers/', include('visitor.urls')),
     path('offices/', include('office.urls')),
